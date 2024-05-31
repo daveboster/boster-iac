f893c4e16ef36842eece7a52d4354f5216567fcc
Dave Boster <dave@boster.dev>
2024-05-31 12:56:38 -0500

    add terraform variables for resource group

--- src/main.tf
+++ src/main.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "boster_web" {
-  name     = TF_VAR_RG_NAME
-  location = TF_VAR_RG_LOCATION
+  name     = var.resource_group_name
+  location = var.resource_group_location
 }
