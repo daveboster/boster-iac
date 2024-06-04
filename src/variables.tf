0b18eae88c3a0eded1c2e4994afe4d2fd6c11658
Dave Boster <dave@boster.dev>
2024-06-04 13:56:36 -0500

    Add New Relic Azure Integration (#2)

--- src/variables.tf
+++ src/variables.tf
@@ -7,4 +7,24 @@ variable "resource_group_location" {
 variable "resource_group_name" {
   type        = string
   description = "Name of the resource group."
+  sensitive   = true
 }
+
+variable "newrelic_account_id" {
+  type        = string
+  description = "The New Relic Account ID."
+  sensitive   = true
+}
+
+variable "newrelic_region" {
+  type        = string
+  description = "The New Relic cloud region."
+  default     = "US"
+}
+
+variable "newrelic_application_name" {
+  type        = string
+  description = "The New Relic application name."
+  sensitive   = true
+}
+
